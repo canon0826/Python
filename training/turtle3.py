8a6ab1cecf3aa333816787b917afbe38eb75c72f
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-04-01 22:23:52 +0900

    graphics3

---
+++ training/turtle3.py
@@ -0,0 +1,8 @@
+from turtle import *
+shape("turtle")
+col = ["orange","limegreen","gold","plum","tomato"]
+for i in range(5): #colorを5つ用意
+    color(col[i]) #線の色を変更
+    forward(200)
+    left(144)
+done
