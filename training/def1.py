ec9ecc0513989fc037960b0183bf42351c14a89a
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-04-06 16:18:20 +0900

    Consumption tax calculation program

--- training/def1.py
+++ training/def1.py
@@ -25,3 +25,20 @@ sayhello()
 # def 関数名(引数, 引数2,...):
 #    関数で行う処理
 #    return 戻り値
+
+def postTaxPrice(price):
+    ans = price * 1.1
+    return ans
+
+print(postTaxPrice(120),"円")
+print(postTaxPrice(128),"円")
+print(postTaxPrice(980),"円")
+
+# 出力結果
+# 132.0 円
+# 140.8 円
+# 1078.0 円
+
+# def関数名(引数):と指定
+# 関数名をpostTaxPriceとするならdef postTaxPrice(price):と指定。
+# 作成した関数はpostTaxPrice(本体価格)で呼び出す。
