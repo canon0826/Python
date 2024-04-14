b7717334bbceea4124b64f43809d140928bb2d61
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-04-14 17:34:38 +0900

    画面作成

--- training/displmage.py
+++ training/displmage.py
@@ -2,3 +2,12 @@ import tkinter as tk
 import tkinter.filedialog as fd
 import PIL.Image
 import PIL.ImageTk
+
+root = tk.Tk()
+root.geometry("400x350")
+
+btn = tk.Button(text="ファイルを開く", command = openFile)
+imageLabel = tk.Label()
+btn.pack()
+imageLabel.pack()
+tk.mainloop()
