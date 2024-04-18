b58a6a0be8e38b5e11ec1ece2cd0309fa7c24465
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-04-18 21:41:10 +0900

    修正

--- training/predictDigite.py
+++ training/predictDigite.py
@@ -3,12 +3,14 @@ import sklearn.svm
 import PIL.Image
 import numpy
 
-
+# 画像ファイルを数値リストに変換する
 def imageToData(filename):
 
+# 画像を8x8のグレースケールに変換
   grayImage = PIL.Image.open(filename).convert("L")
   grayImage = grayImage.resize((8,8),PIL.Image.Resampling.LANCZOS)
 
+# 数値リストに変換
   numImage = numpy.asarray(grayImage, dtype = float)
   numImage = 16 - numpy.floor(17 * numImage / 256)
   numImage = numImage.flatten()
