edaa16a426b33a47099e0aa5f15de61068b5fb89
canon0826 <147060718+canon0826@users.noreply.github.com>
2024-04-23 18:37:02 +0900

    predictDigits()関数追加

--- training/chino.py
+++ training/chino.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.filedialog as fd
 import PIL.Image
 import PIL.ImageTk
-
+# 機械学習で使用するモジュール
 import sklearn.datasets
 import sklearn.svm
 import numpy
@@ -21,3 +21,13 @@ numImage = numpy.asarray(grayImage, dtype = float)
 numImage = 16 - numpy.floor(17 * numImage / 256)
 numImage = numImage.flatten()
 return numImage
+# 数字を予測
+def predictDigits(data):
+# 学習用データを読み込む
+digits = sklearn.datasets.load_digits()
+# 機械学習する
+clf = sklearn.svm.SVC(gamma = 0.001)
+clf.fit(digits.data, digits.target)
+# 予測結果を表示
+n = clf.predict([data])
+textLabel.configure(text = "この画像は"+str(n)+"です！")
